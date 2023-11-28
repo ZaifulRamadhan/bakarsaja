bd1e03c05ac875f2b96e99afb25d9830d5683a84
Zaiful Ramadhan <zaifulramadhan131103@gmail.com>
2023-11-28 10:38:05 +0700

    first save

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,11 +1,13 @@
 import Home from '../views/pages/home';
 import FavoriteRestaurants from '../views/pages/favorite';
 import DetailRestaurants from '../views/pages/detail';
+import Favorite from '../views/pages/like';
 
 const routes = {
   '/': Home, // default page
   '/home': Home, // default page
   '/favorite': FavoriteRestaurants,
   '/detail/:id': DetailRestaurants,
+  '/like': Favorite,
 };
 export default routes;
